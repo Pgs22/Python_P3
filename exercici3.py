287cacf6bca1a95e3f2dba95cf4b58fbde9a8e5d
patricia <patricia.gsbcn@gmail.com>
2025-11-15 02:45:35 +0100

    completado

--- exercici3.py
+++ exercici3.py
@@ -3,10 +3,11 @@
 
 #Ejercicio 1
 #Pide un número y muestra un mensaje de bienvenida si el valor es múltiplo de 2 o mayor que 20.
+
 numero = int(input("Escribe un número: "))
-multiplo = numero % 2
-if(multiplo == 0 or numero > 20):
+if(numero % 2 or numero > 20):
     print("Bienvenido")
+
 
 #Ejercicio 2
 #Crea un programa que reciba un número del 1 al 4 y devuelva el día de la semana correspondiente: 
@@ -26,46 +27,42 @@ match numero:
 #Ejercicio 3
 #Pide tres números y determina:
 def ejercicio3():
-    numero1 = int(input("Escribe el primer número: "))
-    numero2 = int(input("Escribe el segundo número: "))
-    numero3 = int(input("Escribe el tercer número: "))
-    #•cuál es el mayor
-    if(numero1 > numero2 and numero1 > numero3):
-        print("El número más alto es el número 1: ", numero1)
-    elif(numero2 > numero3):
-        print("El número más alto es el número 2: ", numero2)
-    elif(numero2 < numero3):
-        print("El número más alto es el número 3: ", numero3)
+    try:
+        numero1 = int(input("Escribe el primer número: "))
+        numero2 = int(input("Escribe el segundo número: "))
+        numero3 = int(input("Escribe el tercer número: "))
+        #Creo una lista para hacer los calculos de mayo a menor
+        numeros = [numero1, numero2, numero3]
+        #•cuál es el mayor
+        mayor = max(numeros)
+        print("El número más alto es el número: ", mayor)
+        #•cuál es el menor
+        menor = min(numeros)
+        print("El número menor es el ", menor)
+        #•cuántos números se repiten
+        count = 0
+        if(numero1 == numero2):
+            count += 1
+        if(numero2 == numero3):
+            count += 1
+        if(numero3 == numero1):
+            count += 1
+        print("El total de números repetidos es: ", count)
+    except ValueError:
+        print("Error, el valor introducido no es un número")
+        return
 
-
-    #•cuál es el menor
-    if(numero1 < numero2 and numero1 < numero3):
-        print("El número menor es el número 1: ", numero1)
-    elif(numero2 < numero3):
-        print("El número menor es el número 2: ", numero2)
-    elif(numero2 > numero3):
-        print("El número menor es el número 3: ", numero3)
-
-
-    #•cuántos números se repiten
-    count = int(0)
-    if(numero1 == numero2):
-        count += 1
-    if(numero2 == numero3):
-        count += 1
-    if(numero3 == numero1):
-        count += 1
-    print(count)
 
 #Ejercicio 4
 #Pide tres números y muéstralos ordenados de mayor a menor.
-def ejercicio4():
-    numero1 = int(input("Escribe el primer número: "))
-    numero2 = int(input("Escribe el segundo número: "))
-    numero3 = int(input("Escribe el tercer número: "))
-    lista = [numero1, numero2, numero3]
-    ordenados = sorted(lista, reverse=True)
-    print(ordenados)
+
+numero1 = int(input("Escribe el primer número: "))
+numero2 = int(input("Escribe el segundo número: "))
+numero3 = int(input("Escribe el tercer número: "))
+lista = [numero1, numero2, numero3]
+ordenados = sorted(lista, reverse=True)
+print(ordenados)
+
 
 #Ejercicio 5
 #Pide un número y multiplícalo por 3 sin usar el operador de multiplicación, utilizando sumas sucesivas.
@@ -88,16 +85,16 @@ def ejercicio6():
         resultado += entero2
     print(resultado)
 
+
 #Ejercicio 7
 #Pide un número repetidamente hasta que el usuario introduzca un valor decimal válido 
 # comprendido entre 0 y 10.
-decimalValido = False
+decimalValido = True
 while decimalValido:
     decimal = float(input("Escribe el primer número: "))
     if decimal >= 0 and decimal <= 10:
-        decimalValido = True
-    else:
         decimalValido = False
+
 
 #Ejercicio 8
 #Define un número entero entre 0 y 10 y pide al usuario que intente adivinarlo.
@@ -105,13 +102,16 @@ while decimalValido:
 #Si acierta, muestra el número y el total de intentos.
 import random
 numero = (random.randrange(0, 11))
-bingo = False
+bingo = True
+intentos = 0
 while bingo:
     numeroUsuario = int(input("Intenta adivinar el número (Pista: El número es entre 0 y 10: "))
+    intentos = 0
     if(numero == numeroUsuario):
-        bingo = True
-    else:
+        print(f"El número es {numero} y el total de intentos fallidos es: {intentos}")
         bingo = False
+    else:
+        intentos += 1
 
 ###########Funciones y control de operaciones
 
@@ -125,34 +125,48 @@ while bingo:
         #oMostrar los números pares.
         #oMostrar la media de los números introducidos.
 
-def menu(opcion):
-    match opcion:
-        case 1:
-            return "Operación 1" + " Multiplicar dos números indicados por el usuario sin usar el operador de multiplicación."
-        case 2:
-            return "Operación 2" + " Determinar el número mayor y el menor de un grupo de números introducidos por el usuario."
-        case 3:
-            return "Operación 3" + " Mostrar los números pares."
-        case 4:
-            return "Operación 4" + " Mostrar la media de los números introducidos."
+def menu():
+    print("MENU:")
+    print("Operación 1: Multiplicar dos números indicados por el usuario sin usar el operador de multiplicación.")
+    print("Operación 2: Determinar el número mayor y el menor de un grupo de números introducidos por el usuario.")
+    print("Operación 3: Mostrar los números pares.")
+    print("Operación 4: Mostrar la media de los números introducidos.")
 
-finalizarPrograma = True
-while finalizarPrograma: #Ejecuta el While mientras sea true
-    menu(opcion)
+#Mostrar pares
+def pares():
+    numeros = int(input("Escribe cuantos números en total: "))
+    numeros_pares = []
+    for i in range(1,numeros+1):
+        numero = int(input(f"Introduce el número {i}: "))
+        if numero % 2 == 0:
+            numeros_pares.append(numero)
+    print(f"Los numeros pares son: {numeros_pares}")
+
+#Media
+def media():
+    numeros = int(input("Escribe cuantos números en total: "))
+    lista_numeros = []
+    for i in range(1,numeros+1):
+        numero = int(input(f"Introduce el número {i}: "))
+        lista_numeros.append(numero)
+    media = sum(lista_numeros) / numeros
+    print(f"La media de los numeros es: {media}")
+
+def operacion(opcion):
+            match opcion:
+                case 1: ejercicio6()
+                case 2: ejercicio3()
+                case 3: pares()
+                case 4: media()
+
+opcionFinalizar = True
+while opcionFinalizar: #Ejecuta el While mientras sea true
+    menu()
     opcion = int(input("Elige una opción del 1 al 4 (0 para salir): "))
     if opcion == 0:
         print("Programa finalizado")
-        finalizarPrograma = False
-    if  opcion > 0 and opcion <= 4:
-        def operacion(opcion):
-            match opcion:
-                case 1:
-                    ejercicio6()
-                case 2:
-                    ejercicio3()
-                case 3:
-                    ejercicio4()
-                case 4:
-                    ejercicio6()
+        opcionFinalizar = False
+    elif opcion > 0 and opcion <= 4:
+        operacion(opcion)
 
 
